8576fda924e6114ad2dc147d714d8d78cf8de837
Sam De Maeyer <sam_demaeyer@hotmail.com>
2021-12-26 12:18:47 +0000

    Add horse

--- lib/animals/index.ts
+++ lib/animals/index.ts
@@ -2,10 +2,12 @@ import { pig } from './pig'
 import { elephant } from './elephant'
 import { aardvark } from './aardvark'
 import { moose } from './moose'
+import { horse } from './horse'
 
 export default {
   pig,
   elephant,
   aardvark,
   moose,
+  horse,
 }
